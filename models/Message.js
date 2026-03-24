eddf5bbbeb4d9f725076ba4244c5d0647f285add
V.SABARISH <sabarishv.22msc@kongu.edu>
2026-03-24 16:39:18 +0530

    backend

--- models/Message.js
+++ models/Message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
   senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  receiverId: { type: mongoose.Schema.Types.ObjectId, required: true },
   text: { type: String, default: "" },
   status: { type: String, enum: ['sent', 'delivered', 'read'], default: 'sent' },
   deleted: { type: Boolean, default: false },
